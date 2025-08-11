202966b92be59fd72371794446d0955780d45a63
hfyt6 <52505507+hfyt6@users.noreply.github.com>
2025-08-11 23:01:55 +0800

    Initial version for simple mapper

---
+++ Demo/hfyt6Mapper.Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using hfyt6Mapper;
+using hfyt6Mapper.Demo;
+
+
+Mapper.Configure<SimpleObjA, SimpleObjB>(p =>
+{
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Name, b => b.Name);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Id, b => b.Id);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Photo, b => b.Photo);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.IsStudent, b => b.IsStudent);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.BrithDate, b => b.BrithDate);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Description, b => b.Description);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Guid, b => b.Guid);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Money, b => b.Money);
+    p.ForMember<SimpleObjA, SimpleObjB>(a => a.Weight, b => b.Weight);
+});
+
+SimpleObjA a = new SimpleObjA
+{
+    Id = 1,
+    Name = "a",
+    Photo = new byte[] {0x3f, 0x4f, 0x2a, 0x1c, 0x5b},
+    IsStudent = true,
+    BrithDate = new DateTime(1111, 2, 3, 4, 55, 6),
+    Description = "a is a good object",
+    Guid = Guid.NewGuid(),
+    Money = 3.141592654,
+    Weight = 2.718281828459045235360287M,
+};
+
+SimpleObjB b =Mapper.Map<SimpleObjA, SimpleObjB>(a);
+
+if (Math.Abs(a.Money - b.Money) > 1e-6 || Math.Abs(a.Weight - b.Weight) > 1e-9M ||
+    a.Id != b.Id || a.Name != b.Name || a.Photo != b.Photo || a.IsStudent != b.IsStudent || a.BrithDate != b.BrithDate || a.Description != b.Description || a.Guid != b.Guid)
+    Console.WriteLine("Incorrect copying object");
+else
+    Console.WriteLine("Copy correctly");
+
+Console.ReadKey();
---
+++ Src/hfyt6Mapper/hfyt6Mapper/Mapper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hfyt6Mapper
+{
+    public class Mapper
+    {
+        private static readonly Dictionary<int, MapperProfile> _mapperCache = new Dictionary<int, MapperProfile>();
+
+        private static int GetKey(Type t1, Type t2) => (int) ((long)t1.GetHashCode() << 4 ^ t2.GetHashCode());
+        private static int GetKey<TSource, TTarget>() => (int) ((long)typeof(TSource).GetHashCode() << 4 ^ typeof(TTarget).GetHashCode());
+        
+
+        public static void Configure(Action<MapperProfile> configure)
+        {
+            var profile = new MapperProfile();
+            configure(profile);
+            int key = GetKey(profile.SourceType, profile.TargetType);
+
+            if(!_mapperCache.ContainsKey(key)) 
+                _mapperCache[key] = profile;
+        }
+
+        public static void Configure<TSource, TTarget>(Action<MapperProfile> configure)
+        {
+            var profile = new MapperProfile();
+            profile.CreateMap<TSource, TTarget>();
+            configure(profile);
+            int key = GetKey(profile.SourceType, profile.TargetType);
+
+            if (!_mapperCache.ContainsKey(key))
+                _mapperCache[key] = profile;
+        }
+
+        public static TTarget Map<TSource, TTarget>(TSource source)
+        {
+            if (source == null)
+                return default;
+
+            int key = GetKey<TSource, TTarget>();
+
+            if (!_mapperCache.ContainsKey(key))
+            {
+                throw new InvalidOperationException($"No mapping defined for {typeof(TSource)} to {typeof(TTarget)} !");
+            }
+
+            return (TTarget)MapObject(source, _mapperCache[key]);
+        }
+
+        private static object MapObject(object source, MapperProfile profile) 
+        { 
+            var targetObj = Activator.CreateInstance(profile.TargetType);
+
+            foreach(var (sourceValueGetter, targetProperty) in profile.PropertyMappings)
+            {
+                if(targetProperty.CanWrite)
+                {
+                    object val = sourceValueGetter(source);
+                    targetProperty.SetValue(targetObj, ConvertValue(val, targetProperty.PropertyType));
+                }
+            }
+
+            return targetObj;
+        }
+
+        private static object ConvertValue(object value, Type targetType)
+        {
+            Type sourceType = value.GetType();
+            if (value == null || targetType == sourceType)
+                return value;
+
+            if ((targetType == typeof(string) && sourceType.IsPrimitive) ||
+                (sourceType == typeof(string) && targetType.IsPrimitive) ||
+                (targetType.IsPrimitive && sourceType.IsPrimitive))
+                return Convert.ChangeType(value, targetType);
+
+
+            int key = GetKey(sourceType, targetType);
+            if(_mapperCache.ContainsKey(key))
+                return MapObject(value, _mapperCache[key]);
+
+            throw new InvalidOperationException($"Cannot convert {value.GetType()} to {targetType}");
+        }
+    }
+}
